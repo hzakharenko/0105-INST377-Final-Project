cae8532735b1c8df669193325d64525e46a59119
Hanna Zakharenko <hanna@zakharenko.com>
2023-04-30 23:04:26 -0400

    filters but no results showing up

--- scripts.js
+++ scripts.js
@@ -2,30 +2,6 @@
 // Set chartData as a global variable for easy access
 let chartData = [];
 
-// Function to handle the filter button click event
-function handleFilterClick() {
-  // Get the user's input from the form field
-  const majorInput = document.getElementById('major').value;
-
-  // Filter the chart data based on the user's input
-  const filteredData = filterChartData(chartData, majorInput);
-
-  // Render the chart with the filtered data
-  drawBarChart(filteredData);
-}
-
-// Function to filter the chart data
-function filterChartData(data, majorInput) {
-  let filteredData = [];
-
-  // Check if data is an array
-  if (Array.isArray(data)) {
-    filteredData = data.filter((item) => item.major === majorInput);
-  }
-
-  return filteredData;
-}
-
 // Get all courses and extract majors
 async function getAllCourses() {
   const allCourses = [];
@@ -49,7 +25,6 @@ async function getAllCourses() {
   chartData = majors; // Store the data in the global variable
   return majors;
 }
-
 
 // Draw bar chart of number of courses per major
 async function drawBarChart() {
@@ -103,17 +78,87 @@ async function drawBarChart() {
   });
 }
 
+async function filterBarChart(filteredData) {
+  // Get the canvas element
+  const canvas = document.getElementById('myChart');
+
+  // Get majors data
+  const majors = filteredData
+
+  // Check if a chart already exists
+  const existingChart = Chart.getChart(canvas);
+  if (existingChart) {
+    existingChart.destroy();
+  }
+
+  // Get chart data
+  const chartData = {
+    labels: Object.keys(majors),
+    datasets: [
+      {
+        label: "Number of courses per major",
+        data: Object.values(majors),
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1
+      }
+    ]
+  };
+
+  // Get chart options
+  const chartOptions = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  };
+
+  // Get chart context
+  const chartContext = document.getElementById("myChart").getContext("2d");
+
+  // Create and render chart
+  const chart = new Chart(chartContext, {
+    type: "bar",
+    data: chartData,
+    options: chartOptions
+  });
+}
+
+
+// Function to filter the chart data
+function filterChartData(data, majorInput) {
+
+  if (majorInput === "") {
+    drawBarChart(data);
+    return;
+  }
+
+  let filteredData = [];
+
+  // Check if data is an array
+  if (Array.isArray(data)) {
+    filteredData = data.filter((item) => item.major === majorInput);
+  }
+
+  return filteredData;
+}
+
+
 // Call the fetchData function to retrieve the data from the API
 getAllCourses();
 
 // Add an event listener to the filter button
 const filterButton = document.getElementById('data_filter');
-//filterButton.addEventListener('click', handleFilterClick);
 
 filterButton.addEventListener('click', () => {
   const majorInput = document.getElementById('major').value;
   const filteredData = filterChartData(chartData, majorInput);
-  drawBarChart(getAllCourses(filteredData));
+  filterBarChart(getAllCourses(filteredData));
 });
 
 // // Call function to draw chart
